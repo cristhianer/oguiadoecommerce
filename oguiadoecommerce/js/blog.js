1b2dbd79c1f489b23970b3673e8ab6729322dad3
Cristhiane T Resende <cris_t_resende@hotmail.com>
2017-03-19 20:53:01 -0300

    Form validation.

--- oguiadoecommerce/js/blog.js
+++ oguiadoecommerce/js/blog.js
@@ -2,4 +2,17 @@ $(document).ready(function () {
     var d = new Date();
     console.log(d);
 
+	$('#email').focusout(function(){
+                $('#email').filter(function(){
+                   var email=$('#email').val();
+                   console.log(email);
+              var emailReg = /^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+            if( !emailReg.test( email ) ) {
+                alert('Please enter valid email');
+                } else {
+                alert('Thank you for your valid email');
+                }
+                })
+    });
+
 });
